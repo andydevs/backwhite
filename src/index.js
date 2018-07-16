d3d91df8b384e3bab3d50d3157a8578aee8a4370
andydevs <akanshul97@gmail.com>
2018-07-15 23:04:38 -0400

    Implemented links in Backwhite menu

--- src/index.js
+++ src/index.js
@@ -7,3 +7,14 @@
  * Created: 7 - 15 - 2018
  */
 import './scss/main.scss'
+import $ from 'jquery'
+
+$(() => {
+    // Dropdown handling
+    $('.bw-dropdown').each((index, element) => {
+        $(element).find('.bw-dropdown-link').click(() => {
+            $(element).find('.bw-dropdown-list').slideToggle(200)
+        })
+    })
+
+})
