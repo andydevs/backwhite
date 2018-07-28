6e98a985e57d225faca5f6927a1803c40538548d
andydevs <akanshul97@gmail.com>
2018-07-27 22:17:31 -0400

    Hid menu button on desktop

--- dist/js/backwhite.js
+++ dist/js/backwhite.js
@@ -1,13 +1,5 @@
 "use strict";
 
-require("hamburgers");
-
-require("./scss/main.scss");
-
-var _jquery = _interopRequireDefault(require("jquery"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 /**
  * Backwhite
  *
@@ -16,17 +8,17 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  * Author:  Anshul Kharbanda
  * Created: 7 - 15 - 2018
  */
-(0, _jquery.default)(function () {
+$(function () {
   // Dropdown handling
-  (0, _jquery.default)('.bw-dropdown').each(function (index, element) {
-    (0, _jquery.default)(element).find('.bw-dropdown-link').click(function () {
-      (0, _jquery.default)(element).find('.bw-dropdown-list').slideToggle(200);
+  $('.bw-dropdown').each(function (index, element) {
+    $(element).find('.bw-dropdown-link').click(function () {
+      $(element).find('.bw-dropdown-list').slideToggle(200);
     });
   }); // Menu button handling
 
-  (0, _jquery.default)('.bw-toggle').click(function () {
-    (0, _jquery.default)('.bw-content').toggleClass('slided');
-    (0, _jquery.default)('.bw-toggle').toggleClass('is-active');
+  $('.bw-toggle').click(function () {
+    $('.bw-content').toggleClass('slided');
+    $('.bw-toggle').toggleClass('is-active');
   });
 });
 //# sourceMappingURL=backwhite.js.map
--- js/index.js
+++ js/index.js
@@ -6,8 +6,6 @@
  * Author:  Anshul Kharbanda
  * Created: 7 - 15 - 2018
  */
-import $ from 'jquery'
-
 $(() => {
     // Dropdown handling
     $('.bw-dropdown').each((index, element) => {
