5f5bade8ad13cf03e9c11b7473589d13265ab69b
andydevs <akanshul97@gmail.com>
2018-07-29 16:59:34 -0400

    Implemented scrollable fixed-width table

--- dist/js/backwhite.js
+++ dist/js/backwhite.js
@@ -10,7 +10,7 @@
  */
 $(function () {
   // Dropdown handling
-  $('.bw-dropdown').each(function (index, element) {
+  $('.bw-dropdown').each(function (_, element) {
     $(element).find('.bw-dropdown-link').click(function () {
       $(element).find('.bw-dropdown-list').slideToggle(200);
     });
@@ -19,6 +19,23 @@ $(function () {
   $('.bw-toggle').click(function () {
     $('.bw-content').toggleClass('is-slided');
     $('.bw-toggle').toggleClass('is-active');
+  }); // Fix table width
+
+  $('table').each(function (_, table) {
+    // Find longest
+    var longest = $(table).find('th,td').map(function (_, cell) {
+      return $(cell).text();
+    }).get().reduce(function (a, b) {
+      return a.length >= b.length ? a : b;
+    }); // Get length of longest
+
+    var font = $(table).find('td').css('font');
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.font = font;
+    var length = ctx.measureText(longest).width; // Set table cell lengths to length of longest
+
+    $(table).find('th,td').css('width', length);
   });
 });
 //# sourceMappingURL=backwhite.js.map
--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@
  */
 $(() => {
     // Dropdown handling
-    $('.bw-dropdown').each((index, element) => {
+    $('.bw-dropdown').each((_, element) => {
         $(element).find('.bw-dropdown-link').click(() => {
             $(element).find('.bw-dropdown-list').slideToggle(200)
         })
@@ -18,5 +18,25 @@ $(() => {
     $('.bw-toggle').click(() => {
         $('.bw-content').toggleClass('is-slided')
         $('.bw-toggle').toggleClass('is-active')
+    })
+
+    // Fix table width
+    $('table').each((_, table) => {
+        // Find longest
+        let longest = $(table)
+            .find('th,td')
+            .map((_, cell) => $(cell).text())
+            .get()
+            .reduce((a, b) => a.length >= b.length ? a : b)
+
+        // Get length of longest
+        let font = $(table).find('td').css('font')
+        let canvas = document.createElement('canvas')
+        let ctx = canvas.getContext('2d')
+        ctx.font = font
+        let length = ctx.measureText(longest).width
+
+        // Set table cell lengths to length of longest
+        $(table).find('th,td').css('width', length)
     })
 })
