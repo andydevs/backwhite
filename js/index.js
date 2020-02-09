9c9f3852dbe3fe0d8cdbba3e862d5eb910a50ca2
andydevs <akanshul97@gmail.com>
2020-02-09 01:59:49 -0500

    Got rid of the one function function and used an arrow function

--- js/index.js
+++ js/index.js
@@ -24,22 +24,22 @@ $(() => {
     })
 
     // Dynamic Tables
-    $('.bw-front table').each(function() {
+    $('.bw-front table').each((_, table) => {
         // Find longest
-        let longest = $(this)
+        let longest = $(table)
             .find('th,td')
             .map((_, elem) => $(elem).text())
             .get()
             .reduce((a, b) => a.length >= b.length ? a : b, '')
 
         // Get length of longest
-        let font = $(this).find('td').css('font')
+        let font = $(table).find('td').css('font')
         let canvas = document.createElement('canvas')
         let ctx = canvas.getContext('2d')
         ctx.font = font
         let length = ctx.measureText(longest).width
 
         // Set table cell lengths to length of longest
-        $(this).find('th,td').css('width', length)
+        $(table).find('th,td').css('width', length)
     })
 })
